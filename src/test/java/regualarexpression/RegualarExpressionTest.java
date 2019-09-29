90ea8d131f6a9ef9c0278114c7f0405d40ac3f92
UsagiDrops <kristofferkramer83@gmail.com>
2019-09-29 15:18:27 +0200

    regular expression test

--- src/test/java/regualarexpression/RegualarExpressionTest.java
+++ src/test/java/regualarexpression/RegualarExpressionTest.java
@@ -34,11 +34,32 @@ public class RegualarExpressionTest {
         assertEquals("inna", name.replaceAll("[iAa]nna", "inna"));
         // Replace all except Aa with b
         assertEquals("Abba", name.replaceAll("[^Aa]", "b"));
+
+        String thirdAlphaNumeric = "aBcdef12345";
+        assertEquals("XBXdefXXX45", thirdAlphaNumeric.replaceAll("[a-c1-3]", "X"));
+        assertEquals("XXXdefXXX45", thirdAlphaNumeric.replaceAll("[a-cA-Z1-3]", "X"));
+        // ?i ignore case sensitivity
+        assertEquals("XXXXXXXXX45", thirdAlphaNumeric.replaceAll("(?i)[a-z1-3]", "X"));
+
+        //replace all numbers
+        String numbers = "12345a";
+        assertEquals("XXXXXa", numbers.replaceAll("\\d", "X"));
+        //replace all non digits
+        String nonDigits = "abcde1";
+        assertEquals("XXXXX1", nonDigits.replaceAll("\\D", "X"));
+
+        String newLineWhitespaceBlank = "1blanks   \ta tab and a new line \n";
+        assertEquals("1blanksXXXXaXtabXandXaXnewXlineXX", newLineWhitespaceBlank.replaceAll("\\s", "X"));
+        assertEquals("1blanks   Xa tab and a new line \n", newLineWhitespaceBlank.replaceAll("\\t", "X"));
+        assertEquals("XXXXXXX   \tX XXX XXX X XXX XXXX \n", newLineWhitespaceBlank.replaceAll("\\S", "X"));
+        assertEquals("1blanksXXXXaXtabXandXaXnewXlineXX", newLineWhitespaceBlank.replaceAll("\\W", "X"));
+        assertEquals("XXXXXXX   \tX XXX XXX X XXX XXXX \n", newLineWhitespaceBlank.replaceAll("\\w", "X"));
+        assertEquals("X1blanksX   \tXaX XtabX XandX XaX XnewX XlineX \n", newLineWhitespaceBlank.replaceAll("\\b", "X"));
     }
 
     @Test
     public void matchesTest() {
-        String alphanumeric = "abcdefghijk";
+        String alphanumeric = "abcdfghijk";
         assertFalse(alphanumeric.matches("^abcdf"));
         // false since its not the complete String
         assertFalse(alphanumeric.matches("^abcdef"));
