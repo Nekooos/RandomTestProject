e9c1ee5084d26373bbb496dea15aa4031d5de3df
UsagiDrops <kristofferkramer83@gmail.com>
2019-10-25 22:48:15 +0200

    hackerrank

---
+++ src/test/java/hackerrank/Task14.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+//https://www.hackerrank.com/challenges/between-two-sets/problem
+public class Task14 {
+    @Test
+    public void task14() {
+        List<Integer> a = Arrays.asList(2,4);
+        List<Integer> b = Arrays.asList(16,32,96);
+        System.out.println(getTotalX(a, b));
+    }
+
+    private int getTotalX(List<Integer> a, List<Integer> b) {
+
+        return 0;
+    }
+}
